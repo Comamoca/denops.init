de2b2f000f12b7f9d778e8941168971ccc3ae4ab
Comamoca <comamoca.dev@gmail.com>
2023-08-29 09:18:18 +0900

    :beetle: (deps.ts) Change fromFileUrl module url
    Fix to this change to can deno run on URL.

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { fromFileUrl } from "https://deno.land/std@0.200.0/path/mod.ts";
+export { fromFileUrl } from "https://deno.land/std@0.200.0/path/from_file_url.ts";
 export { ensureDir } from "https://deno.land/std@0.200.0/fs/mod.ts";
