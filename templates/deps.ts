0809d20492be3dd75a3fc60dc59858fb6be7a159
Comamoca <comamoca.dev@gmail.com>
2024-02-05 10:33:44 +0900

    :sparkles: (templates/deps.ts) Update dependencies

--- templates/deps.ts
+++ templates/deps.ts
@@ -1,6 +1,6 @@
-export const deps =
-  `export { type Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v5.0.1/helper/mod.ts";
-export { ensure } from "https://deno.land/x/unknownutil@v3.5.1/mod.ts";
-export { is } from "https://deno.land/x/unknownutil@v3.5.1/mod.ts";
+export const deps = `
+export { type Denops } from "https://deno.land/x/denops_std@v6.0.1/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v6.0.1/helper/mod.ts";
+export { ensure } from "https://deno.land/x/unknownutil@v3.14.1/mod.ts";
+export { is } from "https://deno.land/x/unknownutil@v3.14.1/mod.ts";
 `;
