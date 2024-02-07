260d145a583ef888425aa1c9aaca36eaf3e7fcc5
Comamoca <comamoca.dev@gmail.com>
2024-02-07 10:24:21 +0900

    :sparkles: (templates/deps.ts) Unification to URL in import

--- templates/deps.ts
+++ templates/deps.ts
@@ -1,6 +1,5 @@
 export const deps = `
-import { type Denops } from "https://deno.land/x/denops_core@v6.0.5/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v6.0.1/helper/mod.ts";
+export { execute, type Denops } from "https://deno.land/x/denops_std@v6.0.1/helper/mod.ts";
 export { ensure } from "https://deno.land/x/unknownutil@v3.14.1/mod.ts";
 export { is } from "https://deno.land/x/unknownutil@v3.14.1/mod.ts";
 `;
