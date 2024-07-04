6e3a8e2f68486181b9902ca014f267aa63163e10
GitHub <noreply@github.com>
2024-07-04 09:06:08 +0000

    chore(deps): Update deno dependencies

--- templates/deps.ts
+++ templates/deps.ts
@@ -1,5 +1,5 @@
 export const deps = `
-export { execute, type Denops } from "https://deno.land/x/denops_std@v6.5.0/helper/mod.ts";
+export { execute, type Denops } from "https://deno.land/x/denops_std@v7.0.0-pre0/helper/mod.ts";
 export { ensure } from "https://deno.land/x/unknownutil@v3.18.1/mod.ts";
 export { is } from "https://deno.land/x/unknownutil@v3.18.1/mod.ts";
 `;
